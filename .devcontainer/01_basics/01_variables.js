9063c105992f74468af91fc386bb4452883f3eea
samplerforgithub <142079826+samplerforgithub@users.noreply.github.com>
2024-01-02 09:46:29 +0000

    container config

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 226452427
+var accountEmail="himanshi@gmail.com"
+let accountPassword="2565256"
+accountCity="jaipur"
+
+
+//accountId=3 //not allowed
+
+accountEmail="hsjdbu@gmail.com"
+accountPassword="65748"
+accontCity="nagpur"
+var accountName;
+
+/*
+prefer not to use var becoz of issues in block scope and functional scope*/
+
+
+console.log(accountId);
+
+console.table([accountId, accountPassword, accountCity, accountEmail,accountName])
